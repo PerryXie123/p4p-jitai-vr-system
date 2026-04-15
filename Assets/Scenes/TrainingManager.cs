d87477964915fd80f0c932eb644d5bf7432a4a11
Perry Xie <perryxie.nz@gmail.com>
2026-04-15 15:52:49 +1200

    Move menu system to environment and start orb

---
+++ Assets/Scenes/TrainingManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class TrainingManager : MonoBehaviour
+{
+    string trainingMode;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetTrainingMode(string trainingMode)
+    {
+        this.trainingMode = trainingMode;
+    }
+}
